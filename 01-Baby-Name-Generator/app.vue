a2edf4aa238fd7f68afab6b8f5ff792bb7f86b2b
Andrey <boombardir20@gmail.com>
2023-03-18 14:55:19 +0500

    add prettier. fix proj

--- 01-Baby-Name-Generator/app.vue
+++ 01-Baby-Name-Generator/app.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-
-import {Gender, Length, Popularity, names} from "~/data";
+import { Gender, Length, Popularity, names } from '@/data';
 
 interface OptionsState {
   gender: Gender;
@@ -8,25 +7,43 @@ interface OptionsState {
   length: Length;
 }
 
-const option = reactive<OptionsState>({
+const options = reactive<OptionsState>({
   gender: Gender.GIRL,
   popularity: Popularity.TRENDY,
   length: Length.ALL,
-}) // reactive для хранения состояния объекта
+}); // reactive для хранения состояния объекта
 
-const selectedNames = ref<string[]>([]) // ref для хранения состояния массива
+const selectedNames = ref<string[]>([]); // ref для хранения состояния массива
 
 const computeSelectedNames = () => {
-  const filteredNames = names.filter(name => name.gender === option.gender)
-      .filter(name => name.popularity === option.popularity)
-      .filter(name => {
-        if (option.length === Length.ALL) return true
-        else return name.length === option.length
-      })
+  const filteredNames = names
+    .filter((name) => name.gender === options.gender)
+    .filter((name) => name.popularity === options.popularity)
+    .filter((name) => {
+      if (options.length === Length.ALL) return true;
+      else return name.length === options.length;
+    });
 
-  selectedNames.value = filteredNames.map(name => name.name)
-}
+  selectedNames.value = filteredNames.map((name) => name.name);
+};
 
+const optionsArray = [
+  {
+    title: '1) Choose a gender',
+    category: 'gender',
+    buttons: [Gender.GIRL, Gender.UNISEX, Gender.BOY],
+  },
+  {
+    title: '2) Choose the name popularity',
+    category: 'popularity',
+    buttons: [Popularity.TRENDY, Popularity.UNIQUE],
+  },
+  {
+    title: '3) Choose the name length',
+    category: 'length',
+    buttons: [Length.LONG, Length.ALL, Length.SHORT],
+  },
+];
 </script>
 
 <template>
@@ -34,58 +51,39 @@ const computeSelectedNames = () => {
     <h1>Random Baby Name Generator</h1>
     <p>Click the button to generate a random baby name.</p>
     <div class="options-container">
-      <div class="option-container">
-        <h4>1) Choose gender </h4>
-        <div class="option-buttons">
-          <button class="option"
-                  :class="option.gender === Gender.BOY && 'option-active'"
-                  @click="option.gender = Gender.BOY">Boy
-          </button>
-          <button class="option" :class="option.gender === Gender.UNISEX && 'option-active'"
-                  @click="option.gender = Gender.UNISEX">Unisex
-          </button>
-          <button class="option" :class="option.gender === Gender.GIRL && 'option-active'"
-                  @click="option.gender = Gender.GIRL">Girl
-          </button>
-        </div>
-      </div>
-      <div class="option-container">
-        <h4>2) Choose the name popularity</h4>
-        <div class="option-buttons">
-          <button class="option" :class="option.popularity === Popularity.TRENDY && 'option-active'"
-                  @click="option.popularity = Popularity.TRENDY">Trendy
-          </button>
-          <button class="option" :class="option.popularity === Popularity.UNIQUE && 'option-active'"
-                  @click="option.popularity = Popularity.UNIQUE">Unique
-          </button>
-        </div>
-      </div>
-      <div class="option-container">
-        <h4>3) Choose the name length</h4>
-        <div class="option-buttons">
-          <button class="option" :class="option.length === Length.LONG && 'option-active'"
-                  @click="option.length = Length.LONG">Long
-          </button>
-          <button class="option" :class="option.length === Length.ALL && 'option-active'"
-                  @click="option.length = Length.ALL">All
-          </button>
-          <button class="option" :class="option.length === Length.SHORT && 'option-active'"
-                  @click="option.length = Length.SHORT">Short
-          </button>
-        </div>
-      </div>
-      <button class="primary" @click="computeSelectedNames">Generate Name</button>
+      <Option
+        v-for="option in optionsArray"
+        :key="option.title"
+        :option="option"
+        :options="options" />
     </div>
-    <div class="cards-container">
-      <div v-for="name in selectedNames" key="name" class="card">
-        <h4>{{ name }}</h4>
-        <p>x</p>
-      </div>
+    <button class="primary" @click="computeSelectedNames">Generate Name</button>
+  </div>
+  <div class="cards-container">
+    <div v-for="name in selectedNames" key="name" class="card">
+      <h4>{{ name }}</h4>
+      <p>x</p>
     </div>
   </div>
 </template>
 
 <style scoped>
+.option {
+  background-color: rgb(255, 238, 236);
+  color: rgb(27, 60, 138);
+  border: 1px solid rgb(27, 60, 138);
+  border-radius: 6.5rem;
+  padding: 0.75rem 2rem;
+  font-size: 1rem;
+  margin: 0.5rem;
+  cursor: pointer;
+}
+
+.option-active {
+  background-color: rgb(27, 60, 138);
+  color: rgb(255, 238, 236);
+}
+
 .container {
   font-family: Arial, Helvetica, sans-serif;
   color: rgb(27, 60, 138);
@@ -106,22 +104,6 @@ h1 {
   margin: 0 auto;
   margin-top: 4rem;
   position: relative;
-}
-
-.option {
-  background-color: rgb(255, 238, 236);
-  color: rgb(27, 60, 138);
-  border: 1px solid rgb(27, 60, 138);
-  border-radius: 6.5rem;
-  padding: 0.75rem 2rem;
-  font-size: 1rem;
-  margin: 0.5rem;
-  cursor: pointer;
-}
-
-.option-active {
-  background-color: rgb(27, 60, 138);
-  color: rgb(255, 238, 236);
 }
 
 .primary {
---
+++ 01-Baby-Name-Generator/components/Option.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="option-container">
+    <h2>{{ props.option.title }}</h2>
+    <div class="option-buttons">
+      <button
+        v-for="value in props.option.buttons"
+        :key="value"
+        class="option"
+        :class="options[props.option.category] === value && 'option-active'"
+        @click="options[props.option.category] = value">
+        {{ value }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { Gender, Length, Popularity } from '@/data';
+
+interface OptionProps {
+  option: {
+    title: string;
+    category: string;
+    buttons: Gender[] | Popularity[] | Length[];
+  };
+  options: {
+    gender: Gender;
+    popularity: Popularity;
+    length: Length;
+  };
+}
+
+const props = defineProps<OptionProps>();
+</script>
+
+<style scoped>
+.option {
+  background-color: rgb(255, 238, 236);
+  color: rgb(27, 60, 138);
+  border: 1px solid rgb(27, 60, 138);
+  border-radius: 6.5rem;
+  padding: 0.75rem 2rem;
+  font-size: 1rem;
+  margin: 0.5rem;
+  cursor: pointer;
+}
+
+.option-active {
+  background-color: rgb(27, 60, 138);
+  color: rgb(255, 238, 236);
+}
+
+.container {
+  font-family: Arial, Helvetica, sans-serif;
+  color: rgb(27, 60, 138);
+  max-width: 50rem;
+  margin: 0 auto;
+  text-align: center;
+}
+
+h1 {
+  font-size: 3rem;
+}
+
+.options-container {
+  background-color: rgb(255, 238, 236);
+  border-radius: 2rem;
+  padding: 1rem;
+  width: 95%;
+  margin: 0 auto;
+  margin-top: 4rem;
+  position: relative;
+}
+
+.primary {
+  background-color: rgb(27, 60, 138);
+  color: rgb(255, 238, 236);
+  border-radius: 6.5rem;
+  border: none;
+  padding: 0.75rem 4rem;
+  font-size: 1rem;
+  margin-top: 1rem;
+  cursor: pointer;
+}
+
+.cards-container {
+  display: flex;
+  margin-top: 3rem;
+  flex-wrap: wrap;
+}
+
+.card {
+  background-color: rgb(255, 238, 236);
+  border-radius: 2rem;
+  padding: 0.1rem;
+  width: 28%;
+  margin: 0 auto;
+  margin-top: 1rem;
+  position: relative;
+}
+
+.card p {
+  position: absolute;
+  top: 0;
+  left: 92.5%;
+  cursor: pointer;
+  color: black;
+}
+</style>
